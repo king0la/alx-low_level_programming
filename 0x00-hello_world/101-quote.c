12bae0cbdad0edae8d29590c1226b08e5ee84407
king0la <106995297+king0la@users.noreply.github.com>
2022-07-04 23:40:10 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 /**
@@ -6,6 +8,8 @@
  */
 int main(void)
 {
+	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
 	write(2,
 	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 	      s);
