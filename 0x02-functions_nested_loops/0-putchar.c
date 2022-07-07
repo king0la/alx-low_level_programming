9933d4d1ee0f632c9a8c30c986428527483bd2a3
king0la <ayindeolaoluwaa@gmail.com>
2022-07-07 04:41:40 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
- * * main -prints putchar
- * * Return: always 0
- * */
+ * main -prints putchar
+ * Return: always 0
+ */
 int main(void)
 {
 	_putchar('_');
@@ -16,5 +15,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 											
-	return (0);
+		return (0);
 }
