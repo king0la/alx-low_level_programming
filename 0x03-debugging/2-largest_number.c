542883dea398440365585471153e6c0625b0c74d
king0la <106995297+king0la@users.noreply.github.com>
2022-07-08 00:46:25 +0100

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,14 +9,17 @@
  */
 
 int largest_number(int a, int b, int c)
-{
+{   
+    a = 972
+    b = -98
+    c = 0
     int largest;
 
-    if (a > b && b > c)
+    if (a > b && a > c)
     {
         largest = a;
     }
-    else if (b > a && a > c)
+    else if (b > a && b > c)
     {
         largest = b;
     }
