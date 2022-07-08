865b39e4be6dd24e99dff664279ef0c325dbefe4
king0la <ayindeolaoluwaa@gmail.com>
2022-07-07 18:11:12 -0700

    main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,5 +8,6 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 void print_times_tables(int n);
+int main(void);
   
 #endif /* MAIN_H */
